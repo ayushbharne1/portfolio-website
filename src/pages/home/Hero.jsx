cd91a350c48111915bada3bfbc64c6cc231124b1
ayush <ayushbharne@gmail.com>
2026-01-24 13:20:27 +0530

    redirect issue fixed

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -85,7 +85,7 @@ const Hero = () => {
               <div className="absolute inset-0 rounded-full blur-2xl opacity-20 bg-[#ffa116]"></div>
               
               {/* Avatar Circle */}
-              <div className="relative shadow-amber-500 shadow-md overflow-hidden w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64 xl:w-72 xl:h-72 rounded-full flex items-center justify-center text-6xl lg:text-8xl bg-gradient-to-br from-[#2d2d2d] to-[#1a1a1a] border border-[#2d2d2d]">
+              <div className="relative shadow-amber-500 shadow-md overflow-hidden w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64 xl:w-72 xl:h-72 rounded-full flex items-center justify-center text-6xl lg:text-8xl bg-linear-to-br from-[#2d2d2d] to-[#1a1a1a] border border-[#2d2d2d]">
                 <img 
                   src={avatar} 
                   alt="Avatar" 
